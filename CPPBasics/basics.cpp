3981d2342033aa45f3abb7e716baae662f3b4ca3
Shubham Sood <shubham@Shubhams-MacBook-Pro.local>
2025-05-11 13:31:52 +0530

    Initial setup and cpp setup

---
+++ CPPBasics/basics.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+// Below Library includes all the individual libraries
+// #include<bits/stdc++.h>
+ using namespace std;
+
+ int main() {
+   // range -> [-10^9, 10^9]
+    cout << "Hello world!" << endl;
+   //  Int
+    int numIntMin = INT_MIN;
+    cout << numIntMin << endl;
+    int numIntMax = INT_MAX;
+   //  Long
+    long numLong = 11000000000000;
+    cout << numIntMax << endl << numLong;
+
+   //  Float datatype - 7 decimal places
+   float numFloat = 8.72313213213213213;
+   // double - 15 decimal places
+   double numDouble = 8.521365376512765361;
+
+   cout << "Float num - " << numFloat << endl << "Double num - " << numDouble << endl;
+
+   // Storing character
+
+   char ch = 'a';
+   cout << "character is - " << ch << endl;
+
+   // Storing name
+
+   string name = "tuf";
+   cout << name;
+   return 0;
+ }   
